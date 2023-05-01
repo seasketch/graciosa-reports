3f1f290ef847e73a4b1f7ba7bdbc2c2925e978f0
Abby Vath Meyer <avmey@ucsb.edu>
2023-05-01 12:57:26 -0400

    first commit

---
+++ src/functions/clipToOceanEez.ts
@@ -0,0 +1,36 @@
+import {
+  PreprocessingHandler,
+  genPreprocessor,
+} from "@seasketch/geoprocessing";
+import project from "../../project";
+import { genClipLoader } from "@seasketch/geoprocessing/dataproviders";
+
+const clipLoader = genClipLoader(project, [
+  {
+    datasourceId: "global-clipping-osm-land",
+    operation: "difference",
+    options: {
+      unionProperty: "gid",
+    },
+  },
+  {
+    datasourceId: "global-clipping-eez-land-union",
+    operation: "intersection",
+    options: {
+      propertyFilter: {
+        property: "UNION",
+        values: [project.basic.planningAreaName],
+      },
+    },
+  },
+]);
+
+export const clipToOceanEez = genPreprocessor(clipLoader);
+
+export default new PreprocessingHandler(clipToOceanEez, {
+  title: "clipToOceanEez",
+  description: "Example-description",
+  timeout: 40,
+  requiresProperties: [],
+  memory: 4096,
+});
